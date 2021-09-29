5261fbf41a27f64b27f0286865ab9fb9a49bd955
Hetairoi <jonathan90090@gmail.com>
2021-09-29 11:07:52 -0500

    Escritura de archivos

--- src/Newton.java
+++ src/Newton.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -19,71 +21,86 @@ public class Newton {
         Mapa[] valores = Funcion.PedirFuncionNewton();
 
         Mapa[] valoresD = Funcion.Derivada(valores);
+        try {
+            FileWriter fw = new FileWriter("Salida-Newton.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
 
-        do {
-            try {
-                System.out.println("inserte el valor de x");
-                // double x = 1.420990;
-                x = consola.nextDouble();
-                continuar = false;
-            } catch (InputMismatchException e) {
-                System.out.println("Tipo de dato incorrecto: " + e.getMessage());
-                consola.nextLine();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        } while (continuar);
-        continuar = true;
-
-        do {
-            try {
-                System.out.println("Inser el rango de error: ");
-                error = consola.nextDouble();
-                if (error < 0) {
-                    throw new Exception("No se admiten valores negativos.");
+            do {
+                try {
+                    System.out.println("inserte el valor de x");
+                    // double x = 1.420990;
+                    x = consola.nextDouble();
+                    continuar = false;
+                } catch (InputMismatchException e) {
+                    System.out.println("Tipo de dato incorrecto: " + e.getMessage());
+                    consola.nextLine();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
-                continuar = false;
-            } catch (InputMismatchException e) {
-                System.out.println("Tipo de dato incorrecto: " + e.getMessage());
-                consola.nextLine();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        } while (continuar);
-        continuar = true;
-        do {
-            contador++;
-            System.out.println("iteraciones = " + contador);
-            // f = 3 * Math.pow(x, 2) - 17 * x + 20;
-            // f = Math.log(x);
-            f = Funcion.CicloFuncionNewton(valores, x);
+            } while (continuar);
+            continuar = true;
 
-            // System.out.println("fd = " + fd);
-            // f = Math.round(fd*10000000d) / 10000000d;
+            do {
+                try {
+                    System.out.println("Inser el rango de error: ");
+                    error = consola.nextDouble();
+                    if (error < 0) {
+                        throw new Exception("No se admiten valores negativos.");
+                    }
+                    continuar = false;
+                } catch (InputMismatchException e) {
+                    System.out.println("Tipo de dato incorrecto: " + e.getMessage());
+                    consola.nextLine();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            } while (continuar);
+            continuar = true;
+            do {
+                contador++;
 
-            // f1 = 6 * x - 17;
-            // f1 = 1 / x;
-            f1 = Funcion.CicloFuncionNewton(valoresD, x);
+                bw.write("\n" + "iteraciones = " + contador);
+                System.out.println("iteraciones = " + contador);
+                // f = 3 * Math.pow(x, 2) - 17 * x + 20;
+                // f = Math.log(x);
+                f = Funcion.CicloFuncionNewton(valores, x);
 
-            System.out.println("f = " + String.format("%.8f", f));
-            System.out.println("f1 = " + String.format("%.8f", f1));
+                // System.out.println("fd = " + fd);
+                // f = Math.round(fd*10000000d) / 10000000d;
 
-            xr = x - (f / f1);
+                // f1 = 6 * x - 17;
+                // f1 = 1 / x;
+                f1 = Funcion.CicloFuncionNewton(valoresD, x);
 
-            System.out.println("xr = " + String.format("%.8f", xr));
+                bw.write("\n" + "f = " + String.format("%.8f", f));
+                System.out.println("f = " + String.format("%.8f", f));
+                bw.write("\n" + "f1 = " + String.format("%.8f", f1));
+                System.out.println("f1 = " + String.format("%.8f", f1));
 
-            Ea = Math.abs((xr - x) / xr) * 100;
-            System.out.println("Ea = " + String.format("%.10f", Ea) + " %");
+                xr = x - (f / f1);
 
-            if (Ea < error) {
-                bandera = false;
-            }
+                bw.write("\n" + "xr = " + String.format("%.8f", xr));
+                System.out.println("xr = " + String.format("%.8f", xr));
 
-            x = xr;
+                Ea = Math.abs((xr - x) / xr) * 100;
 
-            System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
-        } while (bandera);
+                bw.write("\n" + "Ea = " + String.format("%.10f", Ea) + " %");
+                System.out.println("Ea = " + String.format("%.10f", Ea) + " %");
 
-        consola.close();
+                if (Ea < error) {
+                    bandera = false;
+                }
+
+                x = xr;
+                bw.close();
+                consola.close();
+
+                bw.write("\n" + "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+            } while (bandera);
+        } catch (Exception e) {
+            System.out.println();
+        }
+
     }
 }
--- src/Secante.java
+++ src/Secante.java
@@ -1,5 +1,7 @@
 
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.InputMismatchException;
 
 public class Secante {
@@ -20,87 +22,103 @@ public class Secante {
         int contador = 0;
         Scanner consola = new Scanner(System.in);
         // paso 1
-        do {
-            try {
-                System.out.println("inserte el valor de x1: ");
-                // double x = 1.420990;
-                x1 = consola.nextDouble();
-                continuar = false;
-            } catch (Exception e) {
-                System.out.println("Tipo de dato incorrecto: " + e.getMessage());
-                consola.nextLine();
-            }
-        } while (continuar);
-        continuar = true;
+        try {
+            FileWriter fw = new FileWriter("Salida-Newton.txt");
+            BufferedWriter bw = new BufferedWriter(fw);
 
-        do {
-            try {
-                System.out.println("inserte el valor de xu: ");
-                // double x = 1.420990;
-                xu = consola.nextDouble();
-                continuar = false;
-            } catch (Exception e) {
-                System.out.println("Tipo de dato incorrecto: " + e.getMessage());
-                consola.nextLine();
-            }
-        } while (continuar);
-        continuar = true;
-
-        do {
-            try {
-                System.out.println("Inser el rango de error: ");
-                error = consola.nextDouble();
-                if (error < 0) {
-                    throw new Exception("No se admiten valores negativos.");
+            do {
+                try {
+                    System.out.println("inserte el valor de x1: ");
+                    // double x = 1.420990;
+                    x1 = consola.nextDouble();
+                    continuar = false;
+                } catch (Exception e) {
+                    System.out.println("Tipo de dato incorrecto: " + e.getMessage());
+                    consola.nextLine();
                 }
-                continuar = false;
-            } catch (InputMismatchException e) {
-                System.out.println("Tipo de dato incorrecto: " + e.getMessage());
-                consola.nextLine();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        } while (continuar);
-        continuar = true;
+            } while (continuar);
+            continuar = true;
 
-        double[] valores = Funcion.PedirFuncion();
-        do {
-            System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
-            // f1= Math.pow(x1, 5)+Math.pow(x1, 3)+3;
-            // fu= Math.pow(xu, 5)+Math.pow(xu, 3)+3;
-            // f1 = Math.log(x1);
-            // fu = Math.log(xu);
-            f1 = Funcion.CicloFuncion(valores, x1);
-            fu = Funcion.CicloFuncion(valores, xu);
-            contador++;
-            System.out.println();
-            System.out.println("iteraciones = " + contador);
-            System.out.println("  x1 = " + String.format("%.6f", x1));
-            System.out.println("  xu = " + String.format("%.6f", xu));
-            System.out.println("  f1 = " + String.format("%.6f", f1));
-            System.out.println("  fu = " + String.format("%.6f", fu));
-            n1 = xu - ((xu - x1) / (fu - f1)) * fu;
-            // fx = Math.log(n1);
-            fx = Funcion.CicloFuncion(valores, n1);
-            // fx= Math.pow(n1, 5)+Math.pow(n1, 3)+3;
+            do {
+                try {
+                    System.out.println("inserte el valor de xu: ");
+                    // double x = 1.420990;
+                    xu = consola.nextDouble();
+                    continuar = false;
+                } catch (Exception e) {
+                    System.out.println("Tipo de dato incorrecto: " + e.getMessage());
+                    consola.nextLine();
+                }
+            } while (continuar);
+            continuar = true;
 
-            System.out.println("  n1 = " + String.format("%.6f", n1));
-            System.out.println("  fx = " + String.format("%.6f", fx));
+            do {
+                try {
+                    System.out.println("Inser el rango de error: ");
+                    error = consola.nextDouble();
+                    if (error < 0) {
+                        throw new Exception("No se admiten valores negativos.");
+                    }
+                    continuar = false;
+                } catch (InputMismatchException e) {
+                    System.out.println("Tipo de dato incorrecto: " + e.getMessage());
+                    consola.nextLine();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            } while (continuar);
+            continuar = true;
 
-            Ea = Math.abs((n1 - xu) / n1) * 100;
-            System.out.println("Ea = " + String.format("%.6f", Ea) + "%");
+            double[] valores = Funcion.PedirFuncion();
+            do {
+                bw.write("\n" + "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+                // f1= Math.pow(x1, 5)+Math.pow(x1, 3)+3;
+                // fu= Math.pow(xu, 5)+Math.pow(xu, 3)+3;
+                // f1 = Math.log(x1);
+                // fu = Math.log(xu);
+                f1 = Funcion.CicloFuncion(valores, x1);
+                fu = Funcion.CicloFuncion(valores, xu);
+                contador++;
+                System.out.println();
+                bw.write("\n" + "iteraciones = " + contador);
+                System.out.println("iteraciones = " + contador);
+                bw.write("\n" + "  x1 = " + String.format("%.6f", x1));
+                System.out.println("  x1 = " + String.format("%.6f", x1));
+                bw.write("\n" + "  xu = " + String.format("%.6f", xu));
+                System.out.println("  xu = " + String.format("%.6f", xu));
+                bw.write("\n" + "  f1 = " + String.format("%.6f", f1));
+                System.out.println("  f1 = " + String.format("%.6f", f1));
+                bw.write("\n" + "  fu = " + String.format("%.6f", fu));
+                System.out.println("  fu = " + String.format("%.6f", fu));
+                n1 = xu - ((xu - x1) / (fu - f1)) * fu;
+                // fx = Math.log(n1);
+                fx = Funcion.CicloFuncion(valores, n1);
+                // fx= Math.pow(n1, 5)+Math.pow(n1, 3)+3;
 
-            x1 = xu;
-            xu = n1;
+                bw.write("\n" + "  n1 = " + String.format("%.6f", n1));
+                System.out.println("  n1 = " + String.format("%.6f", n1));
+                bw.write("\n" + "  fx = " + String.format("%.6f", fx));
+                System.out.println("  fx = " + String.format("%.6f", fx));
 
-            System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
-            if (Ea < error) {
+                Ea = Math.abs((n1 - xu) / n1) * 100;
+                bw.write("\n" + "Ea = " + String.format("%.6f", Ea) + "%");
+                System.out.println("Ea = " + String.format("%.6f", Ea) + "%");
 
-                bandera = false;
-            }
+                x1 = xu;
+                xu = n1;
 
-        } while (bandera);
-        consola.close();
+                bw.write("\n" + "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+                System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n");
+                if (Ea < error) {
 
+                    bandera = false;
+                }
+
+            } while (bandera);
+            consola.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
